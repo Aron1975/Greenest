5be0e7cf2075bff7965462e6de2d0129408c2875
Aron Gyulai <arongyf@gmail.com>
2023-10-04 22:46:10 +0200

    Rensat upp, lagt till kommentarer

--- test/Greenest/KottatandeVaxterTest.java
+++ test/Greenest/KottatandeVaxterTest.java
@@ -1,5 +1,6 @@
 package Greenest;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -7,9 +8,14 @@ import static org.junit.jupiter.api.Assertions.*;
 class KottatandeVaxterTest {
 
     Vaxter v = new KottatandeVaxter("Mums", 13);
+    Vaxter v2 = new KottatandeVaxter("NamNam", 1);
+
     @Test
     void raknaUtVatskebehov() {
         assert(v.raknaUtVatskebehov()==0.1 + (0.2*13));
         assert(v.raknaUtVatskebehov()!=0.1 + (0.2*11));
+        assert(v2.raknaUtVatskebehov()==0.1 + (0.2*1));
+        //Assertions.assertEquals(0.3, v2.raknaUtVatskebehov());
+        Assertions.assertEquals(0.1+(0.2*1), v2.raknaUtVatskebehov());
     }
 }
